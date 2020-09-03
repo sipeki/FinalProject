3c11641129d33a91f933e1a76181a2916826a7c1
paullagah <plagah93@gmail.com>
2020-09-03 15:58:46 +0000

    Updated with comments

--- kube.sh
+++ kube.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Building Application
+# Add cluster working to the machine
 aws eks update-kubeconfig --name group1_cluster
+
+# Apply Config Map and Deploy app to Kubernetes Pods
 kubectl apply -f /home/ubuntu/kubernetes_pods/config_map.yml
 kubectl create -f backend_pod.yml
 kubectl create -f frontend_pod.yml
