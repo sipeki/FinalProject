5e695053f0d4f1a8202ca239833196de5d1b14a4
paullagah <plagah93@gmail.com>
2020-09-03 09:49:08 +0000

    Made absolute path for configmap

--- kube.sh
+++ kube.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl apply -f config_map.yml
+kubectl apply -f /home/ubuntu/kubernetes_pods/config_map.yml
 kubectl create -f backend_pod.yml
 kubectl create -f frontend_pod.yml
 kubectl create -f nginx_pod.yml
