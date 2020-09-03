2308a7bca021ff2ec2ba1dabb696db805b3b7b4a
paullagah <plagah93@gmail.com>
2020-09-03 14:12:17 +0000

    Added command to add cluster to the machine

--- kube.sh
+++ kube.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+aws eks update-kubeconfig --name group1_cluster
 kubectl apply -f /home/ubuntu/kubernetes_pods/config_map.yml
 kubectl create -f backend_pod.yml
 kubectl create -f frontend_pod.yml
